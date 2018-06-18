bd3c0a8c10d4b06b59244e459be2f5d751a51e40
Guan xr <1074545235@qq.com>
2018-06-18 15:47:07 +0800

    UserAction添加了修改密码方法，未测试

--- src/com/wlmtxt/User/action/UserAction.java
+++ src/com/wlmtxt/User/action/UserAction.java
@@ -15,7 +15,6 @@ import com.opensymphony.xwork2.ActionSupport;
 import com.wlmtxt.User.service.UserService;
 import com.wlmtxt.domain.DO.wlmtxt_user;
 
-import util.AvatarFile;
 import util.JavaMail;
 import util.JsonUtils;
 
@@ -23,6 +22,15 @@ public class UserAction extends ActionSupport {
 
 	private UserService userService;
 	private wlmtxt_user accpet_user;
+	private String new_password;
+
+	public String getNew_password() {
+		return new_password;
+	}
+
+	public void setNew_password(String new_password) {
+		this.new_password = new_password;
+	}
 
 	public wlmtxt_user getAccpet_user() {
 		return accpet_user;
@@ -39,6 +47,7 @@ public class UserAction extends ActionSupport {
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
+	
 	/**
 	 * 登录<br />
 	 * 1-成功<br />
@@ -57,6 +66,7 @@ public class UserAction extends ActionSupport {
 			pw.write("2");
 		}
 	}
+	
 	/**
 	 * 退出登录,清空session<br>
 	 * 1-成功<br>
@@ -71,6 +81,7 @@ public class UserAction extends ActionSupport {
 		pw.write("1");
 		return "skipToIndexPage";
 	}
+	
 	/**
 	 * 1-验证邮件发送成功<br />
 	 * 2-验证邮件发送失败
@@ -92,6 +103,7 @@ public class UserAction extends ActionSupport {
 			e.printStackTrace();
 		}
 	}
+	
 	/**
 	 * 
 	 * @return 跳转到激活页面
@@ -99,6 +111,7 @@ public class UserAction extends ActionSupport {
 	public String skipActivatePage() {
 		return "skipActivatePage";
 	}
+	
 	/**
 	 * 确认激活按钮
 	 * <li>1-邮件激活成功
@@ -117,7 +130,9 @@ public class UserAction extends ActionSupport {
 			pw.write("2");
 		}
 	}
+	
 	/**
+	 * 
 	 * 验证邮箱是否已注册<br>
 	 * 1-未注册过<br />
 	 * 2-注册过<br />
@@ -134,6 +149,7 @@ public class UserAction extends ActionSupport {
 			pw.write("2");
 		}
 	}
+	
 	/**
 	 * 判断是否已经登录<br>
 	 * 
@@ -150,6 +166,63 @@ public class UserAction extends ActionSupport {
 			pw.write("2");
 		}
 	}
+	
+	/**
+	 * 修改个人资料<br>
+	 * 
+	 * 接收用户显式输入框的所有参数<br>
+	 * 
+	 * 1-修改失败<br>
+	 * 2-修改成功
+	 * @throws IOException 
+	 * 
+	 */
+	public void modifyPersonalData() throws IOException {
+		HttpServletResponse response = ServletActionContext.getResponse();
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		String user = userService.modifyPersonalData(accpet_user);
+		if ("1".equals(user)) {
+			pw.write("1");
+		} else {
+			pw.write("2");
+		}
+		
+	}
+	
+	/**
+	 * 登录后修改密码<br>
+	 * 
+	 * 接收参数，新密码:new_password<br>
+	 * 
+	 * 1-修改成功<br>
+	 * 2-修改失败<br>
+	 * 3-原密码错误，修改失败<br>
+	 * @throws IOException 
+	 */
+	public void modifyPassword() throws IOException {
+		HttpServletResponse response = ServletActionContext.getResponse();
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter pw = response.getWriter();
+		wlmtxt_user user = (wlmtxt_user) ActionContext.getContext().getSession().get("loginResult");
+		if (accpet_user.getUser_password().equals(user.getUser_password())) {
+			user.setUser_password(new_password);
+			String result = userService.modifyPassword(user);
+			if ("1".equals(result)) {
+				pw.write("1");
+			} else {
+				pw.write("2");
+			}
+		} else {
+			pw.write("3");
+		}
+	}
+	
+	
+	public String skipToModifyPasswordPage() {
+		return "skipToModifyPasswordPage";
+	}
+	
 	/**
 	 * 得到用户头像
 	 * @throws IOException 
@@ -194,13 +267,6 @@ public class UserAction extends ActionSupport {
 	 * TODO
 	 */
 	public void 	followUser() {
-		
-	}
-	/**
-	 * 修改密码
-	 * TODO
-	 */
-	public void modifyPassword() {
 		
 	}
 	
