0a594dae69929bc84ef8e6e712b41b8af3a9b166
lyqqer <33982816+lyqqer@users.noreply.github.com>
2018-06-21 00:20:48 +0800

    ...

--- WebContent/js/index/index_category.js
+++ WebContent/js/index/index_category.js
@@ -85,7 +85,7 @@ $(function() {
 				a_div1_div2 = document.createElement("div");
 
 				a_div1_img.className = "video_img";
-				/* a_div1_img.src="css/zb/img/4.png"; */
+				
 				a_div1_div1.className = "video_overplay";
 				a_div1_div2.className = "video_play";
 
@@ -125,10 +125,12 @@ $(function() {
 
 				a_div2_div1.innerHTML = list_vo[num].works.works_title;// 得到作品标题
 				a_div2_div2.innerHTML = "425";// 得到作品浏览量
-				a_div2_div3.innerHTML = list_vo[num].works.works_name;// 得到作品用户名
+				/*a_div2_div3.innerHTML = list_vo[num].user.user_username;// 得到作品用户名
+*/				
+				a_div1_img.src="/wlmtxt/Works/Works_getImg?imgName="+list_vo[num].works.works_cover;
 
-				li_div_div_a.innerHTML = list_vo[num].keyWordDTOList.keyword.keyword_name;
-
+				/*li_div_div_a.innerHTML = list_vo[num].keyWordDTOList.keyword.keyword_name*/
+				
 			}
 		}
 	}
@@ -187,91 +189,93 @@ function getSecondList(first_menu_id, pageIndex) {
 			for (var i = 0; i < long; i++) {
 				old_li[0].parentNode.removeChild(old_li[0]);
 			}
-			/*
-			 * for ( var num=0; num< SecondList_vo.length;num++) {
-			 * 
-			 * 获得一级分类作品列表 var ul_li=null;//ul下的li var li_a=null;//li下的a var
-			 * li_div=null;//li下的div var
-			 * llls_list_container=document.getElementById("llls_list_container");//得到ul的id
-			 * 
-			 * ul_li=document.createElement("li");
-			 * li_a=document.createElement("a");
-			 * li_div=document.createElement("div");
-			 * 
-			 * ul_li.className="list_video_item"; ul_li.style.margin=" 0 5px
-			 * 20px 5px"; li_a.className="video_list_item_wrap";
-			 * li_div.className = "video_label";
-			 * 
-			 * ul_li.appendChild(li_a); ul_li.appendChild(li_div);
-			 * llls_list_container.appendChild(ul_li);
-			 * 
-			 * var a_div1=null;//a下的div1 var a_div2=null;//a下的div2
-			 * a_div1=document.createElement("div");
-			 * a_div2=document.createElement("div");
-			 * a_div1.className="video_cover"; a_div2.className="video_info";
-			 * 
-			 * li_a.appendChild(a_div1); li_a.appendChild(a_div2);
-			 * 
-			 * var a_div1_img=null;//a下的div1下的Img var
-			 * a_div1_div1=null;//a下的div1下的div1 var
-			 * a_div1_div2=null;//a下的div1下的div2
-			 * 
-			 * a_div1_img=document.createElement("img");
-			 * a_div1_div1=document.createElement("div");
-			 * a_div1_div2=document.createElement("div");
-			 * 
-			 * a_div1_img.className="video_img";
-			 * a_div1_img.src="css/zb/img/4.png";
-			 * a_div1_div1.className="video_overplay";
-			 * a_div1_div2.className="video_play";
-			 * 
-			 * a_div1.appendChild(a_div1_img); a_div1.appendChild(a_div1_div1);
-			 * a_div1.appendChild(a_div1_div2);
-			 * 
-			 * var a_div2_div1=null;//a下的div2的div1 var
-			 * a_div2_div2=null;//a下的div2的div2 var
-			 * a_div2_div3=null;//a下的div2的div3
-			 * 
-			 * a_div2_div1=document.createElement("div");
-			 * a_div2_div2=document.createElement("div");
-			 * a_div2_div3=document.createElement("div");
-			 * 
-			 * a_div2_div1.className="video_title";
-			 * a_div2_div2.className="video_number";
-			 * a_div2_div3.className="video_username";
-			 * 
-			 * a_div2.appendChild(a_div2_div1); a_div2.appendChild(a_div2_div2);
-			 * a_div2.appendChild(a_div2_div3);
-			 * 
-			 * var li_div_div=null;//li下的div下的div var
-			 * li_div_div_a=null;//li下的div下的div下的a
-			 * 
-			 * li_div_div=document.createElement("div");
-			 * li_div_div_a=document.createElement("a");
-			 * 
-			 * li_div_div.className="video_label_content";
-			 * li_div_div_a.className="video_label_item";
-			 * 
-			 * li_div_div_a.innerHTML="绝地求生";
-			 * 
-			 * li_div_div.appendChild(li_div_div_a);
-			 * li_div.appendChild(li_div_div);
-			 * 
-			 * a_div2_div1.innerHTML=SecondList_vo[num].works.works_title;//得到作品标题
-			 * a_div2_div2.innerHTML="425";//得到作品浏览量
-			 * a_div2_div3.innerHTML=SecondList_vo[num].works.works_name;//得到作品用户名
-			 * 
-			 * var list_keyword=list_vo[num].keyWordDTOList;
-			 * li_div_div_a.innerHTML=list_keyword[num].keyword.keyword_name
-			 * 
-			 * li_div_div_a.innerHTML=SecondList_vo[num].keyWordDTOList.keyword.keyword_name;
-			 * console.log(SecondList_vo[num].keyWordDTOList.keyword.keyword_name);
-			 */
+			
+			  for ( var num=0; num< SecondList_vo.length;num++) {
+			  
+			  /*获得一级分类作品列表 */
+			  var ul_li=null;//ul下的li var li_a=null;//li下的a var
+			  li_div=null;//li下的div var
+			  llls_list_container=document.getElementById("llls_list_container");//得到ul的id
+			  
+			  ul_li=document.createElement("li");
+			  li_a=document.createElement("a");
+			  li_div=document.createElement("div");
+			  
+			  ul_li.className="list_video_item"; 
+			  ul_li.style.margin=" 0 5px20px 5px"; 
+			  li_a.className="video_list_item_wrap";
+			  li_div.className = "video_label";
+			  
+			  ul_li.appendChild(li_a); ul_li.appendChild(li_div);
+			  llls_list_container.appendChild(ul_li);
+			  
+			  var a_div1=null;//a下的div1 var a_div2=null;//a下的div2
+			  a_div1=document.createElement("div");
+			  a_div2=document.createElement("div");
+			  a_div1.className="video_cover"; a_div2.className="video_info";
+			  
+			  li_a.appendChild(a_div1); li_a.appendChild(a_div2);
+			  
+			  var a_div1_img=null;//a下的div1下的Img var
+			  a_div1_div1=null;//a下的div1下的div1 var
+			  a_div1_div2=null;//a下的div1下的div2
+			  
+			  a_div1_img=document.createElement("img");
+			  a_div1_div1=document.createElement("div");
+			  a_div1_div2=document.createElement("div");
+			  
+			  a_div1_img.className="video_img";
+			  a_div1_img.src="css/zb/img/4.png";
+			  a_div1_div1.className="video_overplay";
+			  a_div1_div2.className="video_play";
+			  
+			  a_div1.appendChild(a_div1_img); a_div1.appendChild(a_div1_div1);
+			  a_div1.appendChild(a_div1_div2);
+			  
+			  var a_div2_div1=null;//a下的div2的div1 var
+			  a_div2_div2=null;//a下的div2的div2 var
+			  a_div2_div3=null;//a下的div2的div3
+			  
+			  a_div2_div1=document.createElement("div");
+			  a_div2_div2=document.createElement("div");
+			  a_div2_div3=document.createElement("div");
+			  
+			  a_div2_div1.className="video_title";
+			  a_div2_div2.className="video_number";
+			  a_div2_div3.className="video_username";
+			  
+			  a_div2.appendChild(a_div2_div1); a_div2.appendChild(a_div2_div2);
+			  a_div2.appendChild(a_div2_div3);
+			  
+			  var li_div_div=null;//li下的div下的div var
+			  li_div_div_a=null;//li下的div下的div下的a
+			  
+			  li_div_div=document.createElement("div");
+			  li_div_div_a=document.createElement("a");
+			  
+			  li_div_div.className="video_label_content";
+			  li_div_div_a.className="video_label_item";
+			  
+			  li_div_div_a.innerHTML="绝地求生";
+			  
+			  li_div_div.appendChild(li_div_div_a);
+			  li_div.appendChild(li_div_div);
+			  
+			  a_div2_div1.innerHTML=SecondList_vo[num].works.works_title;//得到作品标题
+			  a_div2_div2.innerHTML="425";//得到作品浏览量
+			  a_div2_div3.innerHTML=SecondList_vo[num].works.works_name;//得到作品用户名
+			  
+			  var list_keyword=list_vo[num].keyWordDTOList;
+			  li_div_div_a.innerHTML=list_keyword[num].keyword.keyword_name
+			  
+			  li_div_div_a.innerHTML=SecondList_vo[num].keyWordDTOList.keyword.keyword_name;
+			  console.log(SecondList_vo[num].keyWordDTOList.keyword.keyword_name);
+			 
 
-			/* } */
+			 } 
 		}
 	}
-	xhrhp.open("POST", "/wlmtxt/Works/Works_listWorksByFirstMenuID");
+	xhrhp.open("POST", "/wlmtxt/Works/Works_listWorksBySecondMenuID");
 	var formData = new FormData();
 	formData.append("first_menu.first_menu_id", first_menu_id);
 	formData.append("myWorksVO.pageIndex", pageIndex);
