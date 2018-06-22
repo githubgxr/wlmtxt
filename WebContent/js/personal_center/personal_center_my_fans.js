2811e32dc5964f9f1dcbde8ea42cc5fa7f9e78d8
lyqqer <33982816+lyqqer@users.noreply.github.com>
2018-06-22 22:11:58 +0800

    ...

--- WebContent/js/personal_center/personal_center_my_fans.js
+++ WebContent/js/personal_center/personal_center_my_fans.js
@@ -5,30 +5,38 @@ $(function(){
 	listMyFansByPage(1);
 });
 function listMyFansByPage(pageIndex){
+	
+	/*移除之前的数据*/
+	var user_list = document.getElementsByClassName("user_list");
+	var long = user_list.length;
+	for (var num = 0; num < long; num++) {
+		user_list[0].parentNode.removeChild(user_list[0]);
+	}
+	
 	var xhr=new XMLHttpRequest();
 	var formData=new FormData();
-	formData.append("myAttentionVO.pageIndex",pageIndex);
+	formData.append("myFansVO.pageIndex",pageIndex);
 	xhr.open("POST","/wlmtxt/User/User_listMyFansVO");
 	xhr.send(formData);
 	xhr.onreadystatechange=function(){
 		if(xhr.readyState==4 && xhr.status==200){
 			
-			/*var list_myFans=JSON.parse(xhr.responseText);
-			for(var i=0;i<list_myFans.followDTO.length;i++){
+			var list_myFans=JSON.parse(xhr.responseText);
+			for(var i=0;i<list_myFans.userlist.length;i++){
 				
 				var myFans='<li class="user_list">';
 				myFans+='<div class="user_list_img_div">';
-				myFans+='<img class="user_img" src="/wlmtxt/Works/Works_getImg?imgName=' +list_myFans.followDTO[i].user.user_avatar  +'"/>';
+				myFans+='<img class="user_img" src="/wlmtxt/Works/Works_getImg?imgName=' +list_myFans.userlist[i].user_avatar  +'"/>';
 				myFans+='</div>';
 				myFans+='<div class="user_info">';
-				myFans+='<div class="user_name">'+list_myFans.followDTO[i].user.user_username+'</div>';
-				myFans+='<div class="user_operateing" onclick="withFocus(this.id)" id="'+list_myFans.followDTO[i].user.user_id+'">+ 关注</div>';
-				myFans+='<div class="user_grjj">'+list_myFans.followDTO[i].user.user_bio+'</div>';
+				myFans+='<div class="user_name">'+list_myFans.userlist[i].user_username+'</div>';
+				myFans+='<div class="user_operateing" onclick="withFocus(this.id)" id="'+list_myFans.userlist[i].user_id+'">+ 关注</div>';
+				myFans+='<div class="user_grjj">'+list_myFans.userlist[i].user_bio+'</div>';
 				myFans+='</div>';
 				myFans+='</li>';
 				$(".user_list_ul").append(myFans);
-				
-			}*/
+				console.log(list_myFans.userlist[i].user_bio);
+			}
 		}
 	}
 }
@@ -57,7 +65,7 @@ function withFocus(focus_user_id){
 //全部关注
 function withFocusAll(){
 	var xhrhttp=new XMLHttpRequest();
-	xhrhttp.open("POST","/wlmtxt/Works/Works_noticeAllMyFans");
+	xhrhttp.open("POST","/wlmtxt/User/User_noticeAllMyFans");
 	xhrhttp.send(null);
 	xhrhttp.onreadystatechange=function(){
 		if(xhrhttp.readyState==4&&xhrhttp.status==200){
